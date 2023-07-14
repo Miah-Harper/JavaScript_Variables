e1a60b1911916f59ce62eedfdf2829dfba8baa9e
Miah Harper <harpermiah@yahoo.com>
2023-07-14 13:16:52 -0400

    finished exercise

--- app.js
+++ app.js
@@ -4,7 +4,39 @@ console.log(
 );
 
 // Exercise 1
+let lastName;
+var age;
 
 // Exercise 2
+const firstName = "Miah";
+lastName = "Harper";
+age = 29;
+
+console.log(`my name is ${firstName} ${lastName} and I am ${age} years old`);
 
 // Exercise 3
+let language = "JavaScript";
+let createdYear = 1995;
+let isCaseSensitive = true;
+
+console.log(`${language} was created in ${createdYear} and it's ${isCaseSensitive} that it's case sensitive`);
+
+//Exercise 4
+let price = 19.99;
+let isOnSale = false;
+let salePercentage = 15;
+let stock = 0;
+let inStock = false;
+let selectedSize = "M";
+
+console.log(`I saw that there was a ${selectedSize} sized shirt that I liked a lot and was on sale for ${salePercentage} percent off making it only ${price}! 
+However they had ${stock} in stock.`);
+
+//Exercise 5
+let title = "Name of the Wind";
+let author = "Patric Rothfuss";
+let pageCount = 722;
+let bookmark = 456;
+let hasRead = true;
+
+console.log(`You should read the book ${title} by ${author}! It has ${pageCount} pages and I am currently on page ${bookmark}`);
